9de5abc233c663e683fdc91d5723457ab49819fe
Rescor <rescorent@gmail.com>
2019-11-25 23:36:00 +0300

    Working build
    The first working build.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import winter
+import pyowm
 
 winter.load()
+
+city = winter.config['WINTER']['City']
+owm = pyowm.OWM(winter.config['WINTER']['api'], language='en')
+
+observation = owm.weather_at_place(city)
+w = observation.get_weather()
+weather = w.get_detailed_status()
+temperature = w.get_temperature("celsius")
+
+print("The temperature in " + city + " is " + str(int(temperature['temp'])) + "℃, " + weather + ".")
--- winter.py
+++ winter.py
@@ -1,15 +1,30 @@
 import configparser
 
 config = configparser.ConfigParser()
+config.read("config.ini")
+
+
+def load():
+    config_exists_check()
+
+    if 'WINTER' not in config:
+        setup()
+
 
 def setup():
-    lang = input("> ")
-    config['WINTER'] = {'lang': lang}
+    api = input("Write your OWM API key > ")
+    city = input("What is your city? > ")
+    config['WINTER'] = {'api': api,
+                        'city': city
+                        }
 
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
-def load():
-    if 'winter' not in config:
-        setup()
 
+def config_exists_check():
+    try:
+        file = open("config.ini")
+    except FileNotFoundError:
+        file = open("config.ini", "w")
+    file.close()
